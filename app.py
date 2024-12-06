d4ca0c579599dd1fb4476a6f69c7d9ec654782f8
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2024-12-05 16:34:06 -0800

    created main interface

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ st.title("AI Writing Tool")
 
 # LLM (Model)
 llm = ChatGroq(groq_api_key=st.secrets["GROQ_API_KEY"], model="llama3-8b-8192")
+
+# Prompt Template
+prompt_template = ChatPromptTemplate([("system", "You're an expert at text summarizing"), ("user", "Summarize this text: {text}")])
+
+# Output Parser
+parser = StrOutputParser()
+
+# Chain
+chain = prompt_template | llm | parser 
+
+# create a session for summarized text
+st.session_state["summarized_text"] = ""
+
+with st.form("summarize_form", clear_on_submit=True):
+  # Text
+  text_to_summarize = st.text_area('Enter text to summarize')
+  submitted = st.form_submit_button('Submit')
