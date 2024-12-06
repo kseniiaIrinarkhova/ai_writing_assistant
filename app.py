823cb00c88ae51cd05f99740e8211a673eacc593
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2024-12-05 16:30:34 -0800

    Connected to Groq

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+
+# Page Config
+st.set_page_config(page_title="AI Writing Tool")
+
+# Page title
+st.title("AI Writing Tool")
+
+# LLM (Model)
+llm = ChatGroq(groq_api_key=st.secrets["GROQ_API_KEY"], model="llama3-8b-8192")
